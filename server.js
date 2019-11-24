668720e9f6196e0aac8d6ac43508188a48c90c49
Máximo Lanza <54601369+maximolanza@users.noreply.github.com>
2019-11-24 02:23:30 -0300

    Api Test

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 
 
+var express = require('express');
+var app = express();
+
+
+var server = app.listen(8080, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://localhost:%s",port)
+})
+
+
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.get('/api/test', function(req, res) {
+  res.send("Hola mundo!");
+});
 
 
 const AssistantV2 = require('ibm-watson/assistant/v2');
@@ -83,14 +106,6 @@ function processResponse(res) {
   });*/
 }
 
-var express = require('express');
-var app = express();
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.get('/api/:msg', function(req, res) {
   var srt = req.params.msg;
@@ -116,9 +131,3 @@ respuestas.forEach( item => {
     //res.send(srt + 'Sesion obtenida' + SESSIONID_temp);
   });
 
-
-var server = app.listen(8080, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://localhost:%s",port)
-})
