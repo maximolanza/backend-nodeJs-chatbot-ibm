48dfc4b467fc011b37c6b69642886fe34f46a7fd
Máximo Lanza <54601369+maximolanza@users.noreply.github.com>
2019-12-02 00:00:47 -0300

    Dynamic routes setup v2

--- server.js
+++ server.js
@@ -239,8 +239,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
  if(process.env.NODE_ENV === 'production'){
   //set static folder
-  app.use(express.static('client/build'));
-}
+  app.use(express.static(__dirname + '/public'));
+}/*
 app.get('*',(req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+});*/
